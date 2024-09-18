e4cfdec2efe10b1287cb49b1be80d8a23f6c4b94
Axolotian <shum_eu_xuan_ethan@students.edu.sg>
2024-09-18 11:17:42 +0800

    Create Idontknowwhatimdoingimfollwingatutorial.js

---
+++ Idontknowwhatimdoingimfollwingatutorial.js
@@ -0,0 +1,42 @@
+class AxoTest {
+  getInfo() {
+    return {
+      id: 'axotest',
+      name: 'Test',
+      blocks: [
+        {
+          opcode: 'hello',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'test'
+        },        
+        {
+          opcode: 'test',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'test'
+        }.
+        {
+          opcode: 'testwithargs',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'testwithargs[INPUT]'
+          arguments: {
+            INPUT: {
+              type: Scratch.ArgumentType.STRING
+            }
+          }
+        }
+      ]
+    };
+  }
+
+  hello() {
+    return 'Axolotls!';
+  }
+  test() {
+  pass
+  }
+  testwithargs(){
+  	return INPUT;
+  }
+}
+
+Scratch.extensions.register(new AxoTest());
